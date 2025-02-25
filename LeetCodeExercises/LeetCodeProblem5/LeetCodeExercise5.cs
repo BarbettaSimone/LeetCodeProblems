0313b704b17b3731052e7a63a79cff34d0e43226
BarbettaSimone <barbettasimone@gmail.com>
2025-02-25 01:55:38 +0100

    - Added solution to exercise Five of LeetCode (Hybrid solution)

---
+++ LeetCodeExercises/LeetCodeProblem5/LeetCodeExercise5.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeExercises.LeetCodeProblem5
+{
+    internal class LeetCodeExercise5
+    {
+        public LeetCodeExercise5()
+        {
+            Console.WriteLine("Longest Palindromic Substring");
+
+            // read the string
+            Console.WriteLine("Insert the string:");
+            string insertedStr = Console.ReadLine();
+            Console.WriteLine("Inserted string: {0}", insertedStr);
+
+            string palindromicStr = LeetCodeSolution5.FindPalindromicStr(insertedStr);
+            Console.WriteLine("Output: {0}", palindromicStr);
+        }
+    }
+}
---
+++ LeetCodeExercises/LeetCodeProblem5/LeetCodeSolution5.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeExercises
+{
+    internal static class LeetCodeSolution5
+    {
+        public static string FindPalindromicStr(string insertedStr)
+        {
+            string foundStr = string.Empty;
+            char[] listOfChar = insertedStr.ToCharArray();
+            for(int i = 0; i < listOfChar.Length; i++)
+            {
+                List<char> tempChars = new List<char> { listOfChar[i] };
+                int indexBefore = 1;
+                int indexAfter = 1;
+                while (i - indexBefore >= 0 && i + indexAfter < listOfChar.Length)
+                {
+                    if (listOfChar[i - indexBefore] == listOfChar[i + indexAfter])
+                    {
+                        tempChars.Insert(0, listOfChar[i - indexBefore]);
+                        tempChars.Add(listOfChar[i + indexAfter]);
+                        indexBefore++;
+                        indexAfter++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                if (tempChars.Count() > foundStr.Length)
+                {
+                    foundStr = new string(tempChars.ToArray());
+                }
+                tempChars.Clear();
+                indexBefore = 0;
+                indexAfter = 1;
+                while (i - indexBefore >= 0 && i + indexAfter < listOfChar.Length)
+                {
+                    if (listOfChar[i - indexBefore] == listOfChar[i + indexAfter])
+                    {
+                        tempChars.Insert(0, listOfChar[i - indexBefore]);
+                        tempChars.Add(listOfChar[i + indexAfter]);
+                        indexBefore++;
+                        indexAfter++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                if (tempChars.Count() > foundStr.Length)
+                {
+                    foundStr = new string(tempChars.ToArray());
+                }
+            }
+            return foundStr;
+        }
+    }
+}
