02f4dd02a23fd573b071d023a5e8dd6e4b60686a
BarbettaSimone <barbettasimone@gmail.com>
2025-02-25 21:53:57 +0100

    - Added solution to exercise Six of LeetCode

---
+++ LeetCodeExercises/LeetCodeProblem6/LeetCodeExercise6.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeExercises.LeetCodeProblem5
+{
+    internal class LeetCodeExercise6
+    {
+        public LeetCodeExercise6()
+        {
+            Console.WriteLine("ZigZag conversion");
+
+            // read the string
+            Console.WriteLine("Insert the string:");
+            string insertedStr = Console.ReadLine();
+            Console.WriteLine("Insert number of rows");
+            int.TryParse(Console.ReadLine(), out int numRows);
+            if (numRows == 0)
+            {
+                Console.WriteLine("Wrong number of rows inserted");
+                return;
+            }
+
+            Console.WriteLine("Inserted string: {0}", insertedStr);
+            Console.WriteLine("Number of rows: {0}", numRows);
+            string palindromicStr = LeetCodeSolution6.FindZigZag(insertedStr, numRows);
+            Console.WriteLine("Output: {0}", palindromicStr);
+        }
+    }
+}
---
+++ LeetCodeExercises/LeetCodeProblem6/LeetCodeSolution6.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeExercises
+{
+    internal static class LeetCodeSolution6
+    {
+        public static string FindZigZag(string insertedStr, int numRows)
+        {
+            string foundStr = string.Empty;
+            for (int i = 0; i < numRows; i++)
+            {
+                if (i < insertedStr.Length)
+                {
+                    foundStr = foundStr + insertedStr[i];
+                    int x = 0;
+                    while (i + x < insertedStr.Length)
+                    {
+                        bool firstMod = true;
+                        bool secondMod = true;
+                        int increase = (numRows - 1 - i) * 2;
+                        if (increase > 0 && i + x + increase < insertedStr.Length)
+                        {
+                            foundStr = foundStr + insertedStr[i + x + increase];
+                            x = x + increase;
+                        }
+                        else
+                        {
+                            x = x + increase;
+                            firstMod = false;
+                        }
+                        increase = i * 2;
+                        if (increase > 0 && i + x + increase < insertedStr.Length)
+                        {
+                            foundStr = foundStr + insertedStr[i + x + increase];
+                            x = x + increase;
+                        }
+                        else
+                        {
+                            x = x + increase;
+                            secondMod = false;
+                        }
+                        if (firstMod == false && secondMod == false)
+                        {
+                            x = x + numRows;
+                            if (i + x < insertedStr.Length)
+                            {
+                                foundStr = foundStr + insertedStr[i + x];
+                            }
+                        }
+                    }
+                }
+            }
+            return foundStr;
+        }
+    }
+}
